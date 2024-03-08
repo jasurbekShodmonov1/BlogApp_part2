65354f8b596f0bc72507ffd35f16633a8c310af2
jasurbekShodmonov1 <jasurshodmonov21020@gmail.com>
2024-03-08 08:34:55 +0500

    Third Blog app

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
