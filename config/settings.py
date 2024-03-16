511830e1e08be324a3498abad8140815145455c3
jasurbekShodmonov1 <jasurshodmonov21020@gmail.com>
2024-03-16 15:13:55 +0500

    Cors mistake

--- config/settings.py
+++ config/settings.py
@@ -44,17 +44,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'blog',
     'drf_yasg',
     'django_filters',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -150,3 +153,5 @@ REST_FRAMEWORK = {
     )
 
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
